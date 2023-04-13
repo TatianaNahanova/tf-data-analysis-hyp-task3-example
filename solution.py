9e9839639a1200e2f1b6930371887b0d8023e22e
TatianaNahanova <127849913+TatianaNahanova@users.noreply.github.com>
2023-04-13 09:11:52 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 230790372 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+
+def solution(x: np.array, y: np.array) -> bool:
+    alpha = 0.05
+
+    _, pvalue = stats.ttest_ind(x, y, alternative='less')
+
+    if pvalue <= alpha:
+        is_rejected = True
+    else:
+        is_rejected = False
+
+    return is_rejected
