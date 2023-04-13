73cf8ad13f00ac32af6c85d2b93ba7c48f841aa4
TatianaNahanova <127849913+TatianaNahanova@users.noreply.github.com>
2023-04-13 20:58:33 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import permutation_test
+from scipy.stats import mannwhitneyu
 
 
 chat_id = 230790372 # Ваш chat ID, не меняйте название переменной
@@ -9,10 +9,7 @@ chat_id = 230790372 # Ваш chat ID, не меняйте название пе�
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.05
 
-    def statistic(x, y, axis):
-        return np.mean(x, axis=axis) - np.mean(y, axis=axis)
-
-    pvalue = permutation_test([x, y], statistic, alternative='less').pvalue
+    _, pvalue = mannwhitneyu(x, y, alternative="less")
 
     if pvalue <= alpha:
         is_rejected = True
